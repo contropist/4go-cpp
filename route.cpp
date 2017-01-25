87ff6b253644f7eadba2682e2d8b70e6e24fd140
Cool Miller <mianlai.research@gmail.com>
2017-01-25 23:34:55 +0800

    More codes on labor_fly() and route_list().

--- route.cpp
+++ route.cpp
@@ -4,9 +4,62 @@
 #include "def.h"
 #include "object.h"
 
+#include <vector>
+
+// #include <algorithm>
+//
+
+typedef std::vector <pos_list> branches;
+
+pos_list search_next_step(branches fly_route_list, int_type from_code, int_type to_code)
+{
+    pos_list chosen;
+    branches new_list;
+    int_type final_pos;
+
+
+    chosen.push_back(from_code);
+
+}
+
+
+pos_list labor_fly(int_type from_code, int_type to_code)
+// if the chess is a "labor", on the rail assumed
+// a very hard to code module, requires lots of endeavor
+
+{
+    pos_list result;
+
+    return result;
+
+}
+
+
 // route-list modules
-// direct_row(country_type country, row_type row, row_type row2, col_type col)
-//{
+/*
+pos_list direct_row(country_type country, row_type row, row_type row2, col_type col)
+{
+    pos_list line;
+
+    if (row == row2)
+        line.push_back(position(country, row, col).encode());
+    elseif (row2 > row)
+    {
+        line = direct_row(country, row + 1, row2, col);
+        line.push_back(position(country, row, col).encode());
+    }
+    else
+    {
+        reverse(line.begin(), line.end());
+    }
+
+    return line;
+
+}
+*/
+
+
+
 
 
 //
@@ -18,10 +71,10 @@
 pos_list board::route_list(chess_type from_chess, int_type to_code)
 {
     pos_list route;
-    position to(to_code);
-    position from(from_chess.code);
+    int_type from_code = from_chess.code;
+    position from(from_code), to(to_code);
 
-    route.push_back(from_chess.code);
+    route.push_back(from_code);
 
     if ((from.country == to.country) &&
         ((abs(to.row - from.row) + abs(to.col - from.col) == 1) ||
@@ -33,7 +86,7 @@ pos_list board::route_list(chess_type from_chess, int_type to_code)
     if (from.on_rail() && to.on_rail())
     {
         if (from_chess.is_labor())
-            ; //route = labor_fly(from, to);
+            route = labor_fly(from_code, to_code);
         else
         {
             if ((from.country == to.country) &&
